7567ef588e701dc9c38e60df5bfef24d8c599a0d
leovgs <leovgsouza@gmail.com>
2022-06-14 13:27:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let nome = "João"
+
+let sobrenome = "Souza"
+
+let anoNascimento = 1995
+
+let cidade = "Passos-MG"
+
+let anoAtual = 2022
+
+let idade = anoAtual-anoNascimento
+
+console.log (`Eu sou o ${nome} ${sobrenome} tenho ${idade} anos e moro em ${cidade} `)
+
+
+
+
